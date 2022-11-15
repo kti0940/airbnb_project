ea17d749370aa10d642cd4cbe858424ce7be1389
taein Kim <kti0940@gmail.com>
2022-11-15 18:39:33 +0900

    first commit

---
+++ experiences/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class Experience(CommonModel):
+
+    """Experiences Model Definition"""
+
+    country = models.CharField(
+        max_length=50,
+        default="한국",
+    )
+
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+    )
+
+    name = models.CharField(
+        max_length=250,
+    )
+
+    host = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+
+    price = models.PositiveIntegerField()
+    address = models.CharField(
+        max_length=250,
+    )
+
+    start = models.TimeField()
+    end = models.TimeField()
+
+    description = models.TextField()
+    perk = models.ManyToManyField(
+        "experiences.Perk",
+    )
+
+    category = models.ForeignKey(
+        "categories.Category",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Perk(CommonModel):
+
+    """What is included on an Experience"""
+
+    name = models.CharField(
+        max_length=100,
+    )
+    detail = models.CharField(
+        max_length=250,
+        default="",
+        blank=True,
+    )
+    explanation = models.TextField(
+        default="",
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
