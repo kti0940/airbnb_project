436ef22c470e77367584299a735eb9af481fc4fd
taein Kim <kti0940@gmail.com>
2022-11-30 18:21:27 +0900

    Add rooms pagination

--- reviews/models.py
+++ reviews/models.py
@@ -30,4 +30,4 @@ class Review(CommonModel):
     rating = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.user} / {self.rating}⭐️"
+        return f"{self.user} / {self.rating} ⭐️"
